bddc3eaf6767a3c9d383cea46ece41302bd439d9
marovichn <nikolamarovic.11@gmail.com>
2023-07-24 16:23:49 +0200

    Routes-1

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <div><Button>Click me</Button></div>
+}
+
+export default page
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <p className='text-6xl text-purple-500 flex items-center  justify-center w-[100vw] h-[100vh]'>
+      <Button
+        className='p-10 text-5xl text-purple-500 border-purple-500 hover:border-slate-200'
+        variant='outline'
+      >
+        Hiii
+      </Button>
+    </p>
+  );
+}
