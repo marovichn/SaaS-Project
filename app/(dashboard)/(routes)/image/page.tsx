daf3ca0c087ac260a36dd165a6d4f09c05d593ec
marovichn <nikolamarovic.11@gmail.com>
2023-07-26 18:01:01 +0200

    Image-Generation API+Display

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -2,7 +2,7 @@
 
 import Heading from "@/components/Heading";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
-import { ImageIcon } from "lucide-react";
+import { Download, ImageIcon } from "lucide-react";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
@@ -22,6 +22,8 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Card, CardFooter } from "@/components/ui/card";
+import Image from "next/image";
 
 const ImagePage = () => {
   const router = useRouter();
@@ -167,7 +169,25 @@ const ImagePage = () => {
           {images.length === 0 && !isLoading && (
             <Empty label='No images generated ' />
           )}
-          <div>IMAGES WILL BE HERE</div>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8'>
+            {images.map((src) => (
+              <Card key={src} className='rounded-lg overflow-hidden'>
+                <div className='relative aspect-square'>
+                  <Image alt='Image' fill src={src} />
+                </div>
+                <CardFooter className='p-2'>
+                  <Button
+                    onClick={() => window.open(src)}
+                    variant='secondary'
+                    className='w-full'
+                  >
+                    <Download className='h-4 w-4 mr-2' />
+                    Download
+                  </Button>
+                </CardFooter>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
